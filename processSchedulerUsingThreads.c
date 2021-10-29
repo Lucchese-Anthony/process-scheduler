a25faad145f5f00f0f485048b23c6e285f634a5d
Lucchese-Anthony <anthony.lucchese@outlook.com>
2021-10-28 21:18:10 -0400

    Thread version of processScheduler.c
    this is the implementation of processScheduler.c that uses the pthreads.h method

---
+++ processSchedulerUsingThreads.c
@@ -0,0 +1,267 @@
+/* 	Author:			Anthony Lucchese
+	Program Name:		asgn7-lucchesea2.c
+	Compile and run:	gcc -o asgn7 asgn7-lucchesea2.c -Wall -pthread
+
+	Description:	This program is built upon the previous assignment, 
+			doubly linked list, where we add an array of strings
+			and utilize time.h
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <pthread.h>
+
+typedef struct JOB {
+	//init Job struct
+	char command[5][25];
+	long submissionTime;
+	int startTime;
+	struct JOB *prev;
+	struct JOB *next;
+} Job;
+
+typedef struct LIST {
+	//init list struct
+	int numOfJobs;
+	Job *first;
+	Job *last;
+} List;
+
+void *dispatcherThread(void *arg);
+void *createFork(void *arg);
+Job *createJob();
+List *createList();
+void appendJob(List *listPtr, Job *jobPtr);
+void initializeFirstLastJob(List *listPtr, Job *jobPtr); 
+void replaceFirstNode(List *listPtr, Job *jobPtr);
+void insertInMiddle(List *listPtr, Job *jobPtr, int count);
+void insertOrdered(List *listPtr, Job *jobPtr);
+Job *deleteFirstJob(List *listPtr);
+void printList(List *listPtr);
+void printJob(Job *jobPtr);
+
+void *schedulerThread(void *arg) {
+	List *listPtr = arg;
+	char input;
+	while (1) {
+		scanf("%c", &input);
+		if (input == 'p') {
+			printList(listPtr);
+		} else if (input == '+') {
+			Job *jobPtr = createJob();
+			insertOrdered(listPtr, jobPtr);	
+		} else if (input == '-') {
+			if (listPtr->numOfJobs > 0)  {
+				Job *newPtr = deleteFirstJob(listPtr);
+				printJob(newPtr);
+			} else {
+				printf("No Jobs!\n");
+			}
+		}
+	}
+	return NULL;
+}
+
+void *dispatcherThread(void *arg) {
+	long currentSysTime;
+	List *listPtr = arg;
+	Job *nextJob = NULL;
+	while (1) {
+		pthread_t forkCall;
+		if (listPtr->numOfJobs > 0) {
+			currentSysTime = time(NULL);
+			nextJob = listPtr->first;
+			if (currentSysTime >= nextJob->submissionTime + nextJob->startTime) {
+				//printf("SysTime: %ld\n", currentSysTime);       
+				Job *tempJob = deleteFirstJob(listPtr);
+				pthread_create(&forkCall, NULL, createFork, (void*)tempJob);
+				pthread_join(forkCall, NULL);
+				//createFork(tempJob);
+				//printf("Job Completed...\n");
+				free(tempJob);
+			} else {
+				sleep(1);
+			}
+		} else {
+			sleep(1);
+		}
+	}
+	return NULL;
+}
+
+void *createFork(void *arg) {
+	char *argv[4];
+	int status = 999, child_pid;
+	Job *jobPtr = arg;
+	//parent_pid = getpid();
+	for(int i = 0; i <= 4; i++) {
+		if (strcmp("", jobPtr->command[i]) != 0 ) {
+			argv[i] = jobPtr->command[i];
+		} else {
+			argv[i] = NULL;
+		}
+	}
+	child_pid = fork();
+	if (child_pid == 0) {
+		execvp(argv[0], argv);
+		exit(1);
+	} else {
+		waitpid(child_pid, &status, WEXITED);
+	}
+	printJob(jobPtr);
+	free(jobPtr);
+	return NULL;
+}
+
+Job *createJob() {
+	//simplified createJob function
+	Job *jobPtr = (Job*)malloc(sizeof(Job));
+	int input;
+	scanf("%d", &input);
+	for (int i = 0; i < 5; i++) {
+		if (i < input) {
+			scanf("%s", jobPtr->command[i]);
+		} else {
+			//if the length of the desired input is less than 5,
+			//then insert an empty string where there should be
+			//a string, instead of random memory alloc
+			strcpy(jobPtr->command[i], "");
+		}
+	}
+	scanf("%d", &jobPtr->startTime);
+	jobPtr->submissionTime = time(NULL);
+	return jobPtr;
+}
+
+List *createList() {
+	//init the list
+	List *listPtr = (List*)malloc(sizeof(List));
+	listPtr->numOfJobs = 0;
+	listPtr->first = NULL;
+	listPtr->last = NULL;
+	return listPtr;
+}
+
+void appendJob(List *listPtr, Job *jobPtr) {
+	jobPtr->next = NULL;
+	jobPtr->prev = listPtr->last;
+	listPtr->last->next = jobPtr;
+	listPtr->last = jobPtr;
+}
+
+void initializeFirstLastJob(List *listPtr, Job *jobPtr) {
+	listPtr->first = jobPtr;
+	listPtr->last = jobPtr;
+	jobPtr->next = NULL;
+	jobPtr->prev = NULL;
+}
+
+void replaceFirstNode(List *listPtr, Job *jobPtr) {
+	jobPtr->next = listPtr->first;
+	jobPtr->prev = NULL;
+	listPtr->first->prev = jobPtr;		
+	listPtr->first = jobPtr;
+}
+
+void insertInMiddle(List *listPtr, Job *jobPtr, int count) {
+	//there are more 2 or more nodes, and have to find where it goes in 
+	//between the head and tail
+	Job *iterPtr;
+	iterPtr = listPtr->first;
+	while (count >= (iterPtr->submissionTime + iterPtr->startTime) ) {
+		iterPtr = iterPtr->next;
+	}
+	jobPtr->next = iterPtr;
+	jobPtr->prev = iterPtr->prev;
+
+	iterPtr->prev->next = jobPtr;
+	iterPtr->prev = jobPtr;
+}
+
+void insertOrdered(List *listPtr, Job *jobPtr) {
+	int count = jobPtr->startTime + jobPtr->submissionTime;
+	if (listPtr->numOfJobs == 0) {
+		initializeFirstLastJob(listPtr, jobPtr);
+	} else if (count<=listPtr->first->submissionTime+listPtr->first->startTime){
+		replaceFirstNode(listPtr, jobPtr);
+	} else if (count>=listPtr->last->submissionTime+listPtr->last->startTime){
+		appendJob(listPtr, jobPtr);
+	} else {
+		insertInMiddle(listPtr, jobPtr, count);
+	}
+	listPtr->numOfJobs++;
+}
+
+Job *deleteFirstJob(List *listPtr) {
+	Job *newPtr = NULL;
+	if (listPtr->numOfJobs == 1) {
+		newPtr = listPtr->first;
+		printf("Job Deleted:\n");
+		//if the deleted node is the only one,
+		//then remove it and reset all head/tail pointers
+		listPtr->last = NULL;
+		listPtr->first->prev = NULL;
+		listPtr->numOfJobs = 0;
+		listPtr->first = NULL;
+	} else if (listPtr->numOfJobs > 1){
+		newPtr = listPtr->first;
+                printf("Job Deleted:\n");
+		//if there are more than 1,
+		//then delete the first one and reset the head
+		listPtr->first = listPtr->first->next;
+		listPtr->first->prev = NULL;
+
+		listPtr->numOfJobs--;
+	} else {
+		//self explanatory below
+		return newPtr;
+	}
+	return newPtr;
+}
+
+void printList(List *listPtr) {
+	if (listPtr->numOfJobs==0){
+		printf("Empty list!\n");
+	} else {
+		printf("# of jobs: %d\n", listPtr->numOfJobs);
+		Job *ptr = listPtr->first;
+		for (int i = 0; i < listPtr->numOfJobs; i++) {
+			printf("Job %d:\n", i+1);
+			printJob(ptr);
+			ptr = ptr->next;
+		}
+	}
+}
+
+void printJob(Job *jobPtr) {
+	printf("program Name:");
+	int iter = 0;
+	//while the list does not contain any empty strings, print.
+	while(strcmp(jobPtr->command[iter], "") != 0) {
+		printf(" %s", jobPtr->command[iter]);
+		iter++;
+	}
+	printf("\nsubmission Time: %ld\n", jobPtr->submissionTime);
+	printf("start Time: %d\n", jobPtr->startTime);
+}
+
+int main() {
+	List *listPtr;
+	listPtr = createList();
+	pthread_t disp, sched;
+
+	pthread_create(&disp, NULL, schedulerThread,(void*)listPtr);	
+	pthread_create(&sched, NULL, dispatcherThread, (void*)listPtr);
+	pthread_join(sched, NULL);
+	pthread_join(disp, NULL);
+	// scheduler
+	// disbatcherThread
+	printList(listPtr);
+}
